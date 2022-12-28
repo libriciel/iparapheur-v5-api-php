5ae33f8704ee7c99bf17231e3758f72f5ddc6c91
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-12-28 16:49:05 +0100

    Follow review

--- exemples/download-folder-zip.php
+++ exemples/download-folder-zip.php
@@ -1,16 +1,13 @@
 <?php
 
-
 require_once __DIR__ . '/init.php';
 
 use IparapheurV5Client\Api\AdminTrashBin;
 use IparapheurV5Client\Client;
 
-$folderId = 'ede9ac79-8508-11ed-9d51-0242c0a89013';
-
 /** @var Client $client */
 $adminTrashBin = new AdminTrashBin($client);
 
-$result = $adminTrashBin->downloadTrashBinFolderZip($_ENV['TENANT_ID'], $folderId);
+$result = $adminTrashBin->downloadTrashBinFolderZip($_ENV['TENANT_ID'], $_ENV['FOLDER_ID']);
 
 file_put_contents(__DIR__ . '/download/downloadTrashBinFolderZip.zip', $result->getBody()->getContents());
