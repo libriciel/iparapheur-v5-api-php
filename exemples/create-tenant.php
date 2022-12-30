6f22c7000459711aaee3936ef5c178b3eb0420a2
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-12-30 12:00:20 +0100

    Follow review

--- exemples/create-tenant.php
+++ exemples/create-tenant.php
@@ -6,8 +6,6 @@ use IparapheurV5Client\Api\AdminTenant;
 use IparapheurV5Client\Client;
 use IparapheurV5Client\Model\TenantDto;
 
-$folderId = 'ede9ac79-8508-11ed-9d51-0242c0a89013';
-
 $tenantDto = new TenantDto();
 $tenantDto->name = "Mon beau tenant";
 $tenantDto->id = uniqid('', true);
--- exemples/delete-trash-bin-folder.php
+++ exemples/delete-trash-bin-folder.php
@@ -6,8 +6,6 @@ require_once __DIR__ . '/init.php';
 use IparapheurV5Client\Api\AdminTrashBin;
 use IparapheurV5Client\Client;
 
-$folderId = 'ede9ac79-8508-11ed-9d51-0242c0a89013';
-
 /** @var Client $client */
 $adminTrashBin = new AdminTrashBin($client);
-$adminTrashBin->deleteTrashBinFolder($_ENV['TENANT_ID'], $folderId);
+$adminTrashBin->deleteTrashBinFolder($_ENV['TENANT_ID'], $_ENV['FOLDER_ID']);
